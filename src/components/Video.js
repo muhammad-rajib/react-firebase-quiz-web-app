acdfb259757c4a5f3516bc9e2a4854af4d2ca524
Muhammad-Rajib <rajibhossain8521@gmail.com>
2022-05-16 00:39:48 +0600

    updated

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ export default function Video() {
     <a href="quiz.html">
       <div className={classes.video}>
         <img src={image} alt="" />
-        <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+        <p>#23 React Hooks Bangla - React useReducer hooks Bangla</p>
         <div className={classes.qmeta}>
           <p>10 Questions</p>
           <p>Score : Not taken yet</p>
